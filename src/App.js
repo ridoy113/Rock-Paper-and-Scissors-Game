bd27c8656567ea4c0fdb6544e9d3257db3574371
ridoy113 <almamounhridoy113@gmail.com>
2022-08-07 19:35:03 +0600

    start from first

--- src/App.js
+++ src/App.js
@@ -1,48 +1,11 @@
+import logo from './logo.svg';
 import './App.css';
-import Player from './Player';
-
-
-
-const weapons = ["rock", "paper", "scissors"];
 
 function App() {
-
-
-  // state = {
-  //   playerOne: weapons[0],
-  //   plyerTwo: weapons[0],
-  //   winner: ""
-  // }
-
-
   return (
-    <>
-      <h1>Rock Paper Scissors</h1>
+    <div className="App">
 
-
-      <div>
-        <Player />
-      </div>
-
-
-      <div>
-        <button className='weaponBtn'>
-          Rock
-        </button>
-
-        <button className='weaponBtn'>
-          Paper
-        </button>
-
-        <button className='weaponBtn'>
-          Scissors
-        </button>
-      </div>
-
-      <div className='winner'>
-
-      </div>
-    </>
+    </div>
   );
 }
 
