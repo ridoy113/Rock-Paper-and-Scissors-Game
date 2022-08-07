60693d2f560c2eb15c07263ced5b92e655509aa7
ridoy113 <almamounhridoy113@gmail.com>
2022-08-07 18:41:01 +0600

    make all css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Player from './Player';
+
+
+
+const weapons = ["rock", "paper", "scissors"];
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+
+  // state = {
+  //   playerOne: weapons[0],
+  //   plyerTwo: weapons[0],
+  //   winner: ""
+  // }
+
+
+  return (
+    <>
+      <h1>Rock Paper Scissors</h1>
+
+
+      <div>
+        <Player />
+      </div>
+
+
+      <div>
+        <button className='weaponBtn'>
+          Rock
+        </button>
+
+        <button className='weaponBtn'>
+          Paper
+        </button>
+
+        <button className='weaponBtn'>
+          Scissors
+        </button>
+      </div>
+
+      <div className='winner'>
+
+      </div>
+    </>
   );
 }
 
---
+++ src/Player.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Player = () => {
+    return (
+        <div>
+            <h2>plyer</h2>
+        </div>
+    );
+};
+
+export default Player;
